978628c550f39db37c860233c1556b651001cf4d
shaik jahash <shaikjahashahmed9@gmail.com>
2026-06-24 22:06:53 +0530

    Update api.ts

--- src/api/api.ts
+++ src/api/api.ts
@@ -36,7 +36,7 @@ export interface ProductsListResponse {
 /* ---------------- CONFIG ---------------- */
 
 const BASE_URL =
-  import.meta.env.VITE_API_URL || "https://bixonlinestore.onrender.com";
+  import.meta.env.VITE_API_URL || "https://bixonlinestore.onrender.com/api/products";
 
 const TOKEN_KEY = "ml_admin_token";
 const ADMIN_KEY = "ml_admin_profile";
